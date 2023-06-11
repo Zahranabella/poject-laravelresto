611b2893fe6b67151752d13ac264297561f35fbd
Zahranabella <zhranabella@gmail.com>
2023-06-11 22:33:09 +0700

    warna button

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,12 +12,7 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop"
-        viewBox="0 0 16 16">
-        <path
-            d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z" />
-    </svg>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -87,7 +82,7 @@
         }
 
         .form-signin {
-            max-width: 330px;
+            max-width: 430px;
             padding: 15px;
         }
 
@@ -95,7 +90,7 @@
             z-index: 2;
         }
 
-        .form-signin input[type="email"] {
+        .form-signin input[type="name"] {
             margin-bottom: -1px;
             border-bottom-right-radius: 0;
             border-bottom-left-radius: 0;
@@ -106,6 +101,12 @@
             border-top-left-radius: 0;
             border-top-right-radius: 0;
         }
+
+        .form-signin button {
+            background-color: #FC6A73;
+            border-color: #FC6A73;
+            hover : red;
+        }
     </style>
 </head>
 
@@ -113,15 +114,16 @@
 
     <main class="form-signin w-100 m-auto">
         <form class="needs-validation" novalidate action="proses/proses_login.php" method="POST">
-            <i class="bi bi-cup-hot-fill fs-1"></i>
+            <img src="{{ asset('img/logo.jpg') }}">
+            <i class="bi bi-shop fs-1"></i>
             <h1 class="h3 mb-3 fw-normal">Please Login</h1>
 
             <div class="form-floating">
-                <input name="username" type="email" class="form-control" id="floatingInput"
-                    placeholder="name@example.com" required>
-                <label for="floatingInput">Email address</label>
+                <input name="username" type="name" class="form-control" id="floatingInput"
+                    placeholder="Your Username" required>
+                <label for="floatingInput">Username</label>
                 <div class="invalid-feedback">
-                    Masukkan Email yang valid
+                    Masukkan Username yang valid
                 </div>
             </div>
             <div class="form-floating mt-2">
@@ -138,7 +140,7 @@
                     <input type="checkbox" value="remember-me"> Remember me
                 </label>
             </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit_validate"
+            <button class="w-100 btn btn-lg btn-light" type="submit" name="submit_validate"
                 value="abc">Login</button>
         </form>
     </main>
